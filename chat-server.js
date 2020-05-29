e54794bd2decb4c699e1fd683a933ab1291b89b1
jacobwilsonhamilton <jacobwilsonhamilton@gmail.com>
2020-05-29 12:41:02 -0500

    chat stuff

--- chat-server.js
+++ chat-server.js
@@ -1,32 +1,38 @@
-  var app = require('express')();
-  var http = require('http').Server(app);
-  var io = require('socket.io')(http);
+var express = require("express");
+var app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
-  app.get('/', function (req, res) {
-    res.sendfile('chat.html');
+app.use(express.static(__dirname + "/public2"));
+
+app.get("/", function (req, res) {
+  // res.sendfile("chat.css");
+  res.sendfile("./chat2.html");
+});
+let users = [];
+io.on("connection", function (socket) {
+  console.log("A user connected");
+  socket.on("setUsername", function (data) {
+    console.log(data);
+    if (users.indexOf(data) > -1) {
+      socket.emit(
+        "userExists",
+        data + " username is taken! Try some other username."
+      );
+    } else {
+      users.push(data);
+      socket.emit("userSet", {
+        username: data
+      });
+    }
   });
 
-  users = [];
-  io.on('connection', function (socket) {
-    console.log('A user connected');
-    socket.on('setUsername', function (data) {
-      console.log(data);
-      if (users.indexOf(data) > -1) {
-        socket.emit('userExists', data + ' username is taken! Try some other username.');
-      } else {
-        users.push(data);
-        socket.emit('userSet', {
-          username: data
-        });
-      }
-    });
-
-    socket.on('msg', function (data) {
-      //Send message to everyone
-      io.sockets.emit('newmsg', data);
-    })
+  socket.on("msg", function (data) {
+    //Send message to everyone
+    io.sockets.emit("newmsg", data);
   });
+});
 
-  http.listen(3000, function () {
-    console.log('listening on localhost:3000');
-  });
+http.listen(3000, function () {
+  console.log("listening on localhost:3000");
+});
